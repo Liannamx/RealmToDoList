f998754871ce0edfb7bf1ed65bafbcfcb32adf28
Alina Maksimova <liannamx@icloud.com>
2021-03-22 14:05:13 +0300

    Create project

---
+++ RealmToDoList/RealmToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmToDoList
+//
+//  Created by Olina Maksimova on 22.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
